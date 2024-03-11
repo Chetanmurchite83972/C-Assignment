7799a5db46acc701c62d27258aed5d95997f0999
Chetanmurchite <chetanmurchitte@gmail.com>
2024-03-11 16:40:49 +0530

    A4_q8

---
+++ Assignments/Assignment4/C/q8.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int num1=10,num2=20;
+void swap(void);
+
+int main(void)
+{
+ printf("Before swap:num1=%d,num2=%d\n",num1,num2);
+ 
+ swap();
+
+ printf("After swap:num1=%d,num2=%d\n",num1,num2);
+
+ return 0;
+}
+ void swap()
+ {
+  int temp=num1;
+  num1=num2;
+  num2=temp;
+ }
+
+
